e0673f061d1cc6c89ffccfa6dad78cb9cdcc3222
Enes4006 <128158172+Enes4006@users.noreply.github.com>
2024-09-24 19:05:29 +0300

    Updated

--- sayi tahmin.py
+++ sayi tahmin.py
@@ -1,16 +1,19 @@
 import random
 
-rastgelesayi=random.randint(1,100)
+rastgelesayi=random.randint(1,10)
 
+sayac=1
 
 while True:
     sayi=int(input("Bir sayi giriniz: "))
     if(sayi==rastgelesayi):
-        print("Tebrikler kazandiniz")
+        print(f"Tebrikler {sayac} hamlede bildiniz")
         break
     elif(sayi<rastgelesayi):
         print("Yukarı")
+        sayac=sayac+1
     elif(sayi>rastgelesayi):
         print("Aşağı")
+        sayac=sayac+1
     else:
         print("Doğru sayı giriniz")
